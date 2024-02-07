18c786e0510a8e9deb313bae9b815b5b92b1c814
Chiara Sheldon <chiarasheldon@gmail.com>
2024-02-07 16:56:58 -0600

    content CRUD

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,3 +1,6 @@
 class Content < ApplicationRecord
   belongs_to :doctor
+
+  CONTENT_TYPE = %w[Articulo Video Podcast Noticia Infografia Consejo]
+  THEME_TYPE = %w[Salud Nutricion Ejercicio Mentalidad Otro]
 end
