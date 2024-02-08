fb4404a527073e656d94b01aca473b2238d1a072
Chiara Sheldon <chiarasheldon@gmail.com>
2024-02-07 19:02:35 -0600

    cloudinary

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,5 +1,6 @@
 class Content < ApplicationRecord
   belongs_to :doctor
+  has_many_attached :photos
 
   CONTENT_TYPE = %w[Articulo Video Podcast Noticia Infografia Consejo]
   THEME_TYPE = %w[Salud Nutricion Ejercicio Mentalidad Otro]
