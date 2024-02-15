a0d19fc91a17b7719df6cb657b927ccb4c6b5114
Chiara Sheldon <chiarasheldon@gmail.com>
2024-02-15 13:14:06 -0600

    checking the QR code, fixed the answers/questions and added chartkick

--- app/controllers/shared_controller.rb
+++ app/controllers/shared_controller.rb
@@ -3,5 +3,13 @@ class SharedController < ApplicationController
     @patient = current_patient
     @reports = @patient.reports.order(created_at: :desc).limit(5)
     @answers = @reports.map(&:answers).flatten
+    @no_count = Answer.where(patient_id: @patient.id, question_id: 2, text: 'No').count
+    @si_count = Answer.where(patient_id: @patient.id, question_id: 2, text: 'Sí').count
+    @desayuno_count = Answer.where(text: 'Desayuno').count
+    @almuerzo_count = Answer.where(text: 'Almuerzo').count
+    @comida_count = Answer.where(text: 'Comida').count
+    @cena_count = Answer.where(text: 'Cena').count
+    @answers_8 = Answer.where(question_id: 8).pluck(:text)
+    @answers_9 = Answer.where(question_id: 9).pluck(:text)
   end
 end
